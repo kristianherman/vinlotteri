3c133bb5099b6304c20fec0f56b5b258187e8190
kristianherman <55444482+kristianherman@users.noreply.github.com>
2021-07-12 11:50:53 +0200

    Add files via upload

---
+++ kristian_django_app/urls.py
@@ -0,0 +1,27 @@
+"""kristian_django_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from vinlotteri.views import home_view, lodd_trekning_view, lodd_create_view, lodd_delete_view
+
+urlpatterns = [
+    path('', home_view),
+    path('lodd_trekkning/', lodd_trekning_view),
+    path('lodd_create/', lodd_create_view),
+    path('lodd_delete/', lodd_delete_view),
+    path('admin/', admin.site.urls),
+] 
